1ace98bfac7484c4d755e3ff657a293999dbf1f9
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-01-22 09:03:46 -0700

    Initial commit

---
+++ mod3/chars.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chars.c
+ *
+ *    Description:  Learn how to use characters
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 08:53:37 AM
+ *       Revision:  none
+ *       Compiler:  gcc chars.c -o chars.out
+ *
+ *         Author:  Jayden Williams (), jaydenwilliams@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int age = 20;
+    double weight = 178.3;
+    char sex = 'M'; // use single ticks
+
+    printf("Your age is %d and your weight %lf and your sex is %c\n, age, weight, sex);
+
+    return 0;
+}
+// Function Definitions
+
+
