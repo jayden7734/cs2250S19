dcce1a4b80aaed483e12ccaeaf806eb44947c7bb
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-01-29 10:11:11 -0700

    Added switch statement to show curren water state

--- mod4/tempurature.c
+++ mod4/tempurature.c
@@ -18,7 +18,15 @@
 #include <stdio.h>
 
 // Constants
-
+#define CELCIUS 0
+#define FAHRENHEIT 1
+#define BOIL_C 100
+#define BOIL_F 212
+#define FREEZE_C 0
+#define FREEZE_F 32
+#define BOIL 1
+#define LIQUID 2
+#define ICE 3
 // Function Prototypes
 
 // Main Function
@@ -27,32 +35,85 @@ int main()
     int choice;
     double in_temp;
     double out_temp;
-    
+    int water_state = 0;
     printf("Welcome to the tempurature calculator\n");
     printf("Please enter your choice:\n");
     printf("\t0 for Celsius or 1 for Fahrenheit:\n");
     
     scanf("%d", &choice);
 
-    if( choice == 0) {
+    if( choice == FAHRENHEIT) {
         printf("Enter the tempurature in Fahrenheit:\n");
         scanf("%lf",&in_temp);
 
-        out_temp = (in_temp-32)*(5/9);
+        out_temp = (in_temp-32)*(5.0/9.0);
         printf("The tempurature in celcius is %lf\n",out_temp);
     }
-    else if (choice == 1) {
+    else if (choice == CELCIUS) {
         
         printf("Enter the tempurature in celsius:\n");
         scanf("%lf",&in_temp);
-        out_temp = (in_temp * 9/5)+32;
+        out_temp = (in_temp * 9.0/5.0)+32;
         printf("The tempurature in Fahrenheit is %lf\n", out_temp);
 
     }
     else {
-        printf("Sorry, you did not enter zero or one\n");
+        
+        
+        printf("Sorry, you did not enter 0 or 1\n");
     }
-        return 0;
+        
+
+//Task 2
+//Test for boiling point, freezing point, and liquid state
+
+    if(choice == FAHRENHEIT) 
+    {
+        if (in_temp > BOIL_F) 
+        {
+            water_state = BOIL;
+        }
+        else if (in_temp > FREEZE_F && in_temp < BOIL_F)
+        {
+            water_state = LIQUID;
+        }
+        else 
+        { 
+            water_state = ICE;
+        }
+    }
+
+ 
+    else
+    {
+        if (in_temp > BOIL_C) 
+        {
+            water_state = BOIL;
+        }
+    
+        else if (in_temp > FREEZE_C && in_temp < BOIL_C)
+        {
+            water_state = LIQUID;
+        }
+        else
+        {
+            water_state = ICE;
+        }
+    }
+    // Switch
+    switch (water_state)
+    {
+        case BOIL:
+            printf("Your water is boiling\n");
+            break;
+        case LIQUID:    
+            printf("Your water is frozen\n");
+            break;
+        case ICE:
+            printf("Your water is liquid\n");
+            break;
+    }
+            return 0;
 }
 // Function Definitions
 
