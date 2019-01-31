e7158eff295ef6f0a2cf6e4cf8fb2f3759eb2d96
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-01-31 09:47:37 -0700

    Works with constants for months

--- mod4/month_day.c
+++ mod4/month_day.c
@@ -18,13 +18,73 @@
 #include <stdio.h>
 
 // Constants
-
+#define JANUARY     1
+#define FEBRUARY    2
+#define MARCH       3
+#define APRIL       4
+#define MAY         5
+#define JUNE        6
+#define JULY        7
+#define AUGUST      8
+#define SEPTEMBER   9
+#define OCTOBER     10
+#define NOVEMBER    11
+#define DECEMBER    12
 // Function Prototypes
 
 // Main Function
 int main()
 {
+    int month;
+    int day;
 
+    // Capture user input
+    printf("Enter the month (1-12):\n");
+    scanf("%d", &month);
+    printf("Enter the day (1-31):\n");
+    scanf("%d", &day);
+    // Use IF to validate month
+    if (month > 12 || month < 1)
+    {
+        printf("Number is out of range (1-12).\n");
+    }
+    switch(month)
+    {
+        //months with 31 days
+        case JANUARY:
+        case MARCH:
+        case MAY:
+        case JULY:
+        case AUGUST:
+        case OCTOBER:
+        case DECEMBER:
+            if(day >31 || day < 0)
+            {
+                printf("Invalid day\n");
+                return 1;// ends code at this point
+            }
+            break;
+        //months with 30 days
+        case APRIL:
+        case JUNE:
+        case SEPTEMBER:
+        case NOVEMBER:
+            if(day >30 || day < 0)
+            {
+                printf("Invalid day\n");
+                return 1;
+            }
+            break;
+        case FEBRUARY:
+            if(day >28 || day < 0)
+            {
+                printf("Invalid day\n");
+                return 1;
+            }
+            break;
+    }
+        printf("You enter a valid month[%d] and day[%d]\n", month,day);
+    // Use SWITCH to validate the day of month
     return 0;
 }
 // Function Definitions
