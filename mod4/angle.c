2282880c1feeca801579b3c0be6136d7c410ad6d
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-01-31 09:18:04 -0700

    Added results for angles on axis

--- mod4/angle.c
+++ mod4/angle.c
@@ -4,6 +4,10 @@
  *       Filename:  angle.c
  *
  *    Description:  Calculate the quadrant based on the input angle
+ *                  If the angle is 0, print positive x axis
+ *                  If the angle is 90, print positive y axis
+ *                  If the angle is 180, print negative x axis
+ *                  If the angle is 270, print negative y axis
  *
  *        Version:  1.0
  *        Created:  01/31/2019 08:37:33 AM
@@ -54,6 +58,24 @@ int main()
     else if (angle > 270 && angle < 360)
     {
         printf("\nThis is quadrant IV\n");
+    }
+    else if (angle == 0 || angle == 90 || angle == 180 || angle == 270)//on axis
+    {
+        switch(angle)
+        {
+            case 0:
+                printf("\nThe angle is on the positive x axis\n");
+                break;
+            case 90:
+                printf("\nThe angle is on the positive y axis\n");
+                break;
+            case 180:
+                printf("\nThe angle is on the negative x axis\n");
+                break;
+            case 270:
+                printf("\nThe angle is on the negative y axis\n");
+                break;
+        }
     }
     else 
     {
