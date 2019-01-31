70351363ad16d1bfc20df31326fee1926dd5bb52
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-01-31 09:19:40 -0700

    initial commit

---
+++ mod4/month_day.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:18:32 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o month_day.out
+ *
+ *         Author:  Jayden Williams (), jaydenwilliams@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
