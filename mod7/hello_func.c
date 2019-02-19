b64efbc97805922f662cab181876d8cc56ed5f37
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-02-19 11:37:11 -0700

    first commit

--- mod7/hello_func.c
+++ mod7/hello_func.c
@@ -23,15 +23,33 @@
 void Hello_World();
 void Hello_Number(int num);
 int Get_Square(int num);
+int Get_Number();
+int Get_Sum(int num1, int num2);
+double Get_BMI(double weight, double height);
 
 // Main Function
 int main()
 {
     int i1 = 23;
+    int num1, num2;
     Hello_World();
     Hello_Number(i1);
+
     i1 = Get_Square(i1);
     Hello_Number(i1);
+
+    i1 = Get_Number();
+    Hello_Number(i1);
+
+    num1 = Get_Number();
+    Hello_Number(num1);
+    num2 = Get_Number();
+    Hello_Number(num2);
+    // Get_Sum(num1, num2);
+    Hello_Number(Get_Sum(num1, num2));
+
+    double bmi = Get_BMI(65.8, 1.89);
+    printf("Your BMI is[%lf]\n", bmi);
     return 0;
 }
 // Function Definitions
@@ -76,4 +94,51 @@ int Get_Square(int num)
     //Algorithm
     square = num * num;
     return square;
+}
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_Number
+ *  Description:  Ask user to enter an integer
+ *  Param: None
+ *  Return: User int input
+ * =====================================================================================
+ */
+int Get_Number()
+{
+    int userNum;
+    printf("Enter an integer:\n");
+    scanf("%d", &userNum);
+
+    return userNum;
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_Sum
+ *  Description:  Return sum of two int
+ *  Param: int1, int2
+ *  Return: First + Second
+ * =====================================================================================
+ */
+int Get_Sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_BMI
+ *  Description:  Calculate the body max index(weight/height^2)
+ *  param: Weight(kg), Height(m)
+ *  return: BMI
+ *
+ * =====================================================================================
+ */
+double Get_BMI(double weight, double height)
+{
+    double BMI;
+    BMI = weight/(height * height);
+    return BMI;
 }
