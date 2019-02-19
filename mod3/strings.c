d9eb332ce3228bf009e9fd2d39cfb62e802231bc
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-02-19 11:31:11 -0700

    updated

--- mod3/strings.c
+++ mod3/strings.c
@@ -29,16 +29,35 @@ int main()
    char lName[SIZE];
    unsigned int age; // For unsigned use the %du, %lu, %llu
    // Strings use %s as the placeholder
-   printf("Please enter your first name (<%d characters, NO spaces)\n", SIZE);
+   printf("Please enter your first name (<%d characters, NO spaces):\n", SIZE);
    scanf("%s", fName); // when using arrays, do not include the '&' 
-   printf("Enter your age\n");
+   printf("Please enter your last name (<%d characters, NO spaces):\n", SIZE);
+   scanf("%s", lName); // when using arrays, do not include the '&' 
+   printf("Enter your age:\n");
    scanf("%du", &age);
    
-   printf("Hi [%s]", fName);
-   printf("Please enter your last name (<%d characters, NO spaces)\n", SIZE);
-   scanf("%s", lName); // when using arrays, do not include the '&' 
-   printf("[%s][ %s] %d years old", fName,lName,age);
-
+   //printf("Hi [%s]", fName);
+   printf("[%s][ %s] is %d years old.\n", fName,lName,age);
+   if(age >= 24)
+   {
+       printf("You old fart.\n");
+   }
+   else if (age < 24)
+   {
+       printf("Still a young buck.\n");
+   }
+ /*  else if (age < 75 && age >= 65)
+   {
+       printf("Enjoy your retirement.\n");
+   }
+   else if (age < 65 && age > 20)
+   {
+       printf("Nothing special\n");
+   }
+   else
+   {
+       printf("Still a young buck.\n");
+   }*/
     return 0;
 }
 // Function Definitions
