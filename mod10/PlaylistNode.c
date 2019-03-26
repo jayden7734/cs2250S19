877c50783f0ff03f7b069110bf8efc5dd2c9ff1d
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-03-26 10:21:23 -0600

    Skeleton made

--- mod10/PlaylistNode.c
+++ mod10/PlaylistNode.c
@@ -19,47 +19,9 @@
 #include <stdlib.h>
 #include "PlaylistNode.h"
 // Function Definitions
-char PrintMenu(char playlist[])
-{
-    char userInput = '-';
-    char ID[50];
-    char songName[50];
-    char artistName[50];
-    int length;
-
-    printf("%s PLAYLIST MENU\n", playlist);
-    printf("a - Add song\n");
-    printf("r - Remove song\n");
-    printf("c - Change position of song\n");
-    printf("s - Output songs by specific artist\n");
-    printf("t - Output total time of playlist (in seconds)\n");
-    printf("o - Output full playlist\nq - Quit\n\n");
-    while(userInput != 'a'&&userInput != 'r'&&userInput != 'c'&&userInput != 's'
-            &&userInput != 't'&&userInput != 'o'&&userInput != 'q')
-    {
-        printf("Choose an option:\n");
-        scanf(" %c", &userInput);
-    }
-    switch(userInput)
-    {
-        case 'a':
-
-            printf("ADD SONG\n");
-            printf("Enter song's unique ID:\n");
-            scanf(" %s", ID);
-            printf("Enter song's name:\n");
-            scanf(" %s", songName);
-            printf("Enter artist's name:\n");
-            scanf(" %s", artistName);
-            printf("Enter song's length:\n");
-            scanf("%d", length);
-
-
-    return userInput;
-}
-
-void CreatePlaylistNode(PlaylistNode* thisNode, char idInit[], char songNameInit[],
-        char artistNameInit, int songLengthInit, PlaylistNode* nextNode);
+void CreatePlaylistNode(PlaylistNode* thisNode, char idInit[], 
+        char songNameInit[], char artistNameInit, int songLengthInit, 
+        PlaylistNode* nextNode)
 {
     //1) Set all your strings in thisNode to the input parameters (strcpy)
     //2) Set all your integers in thisNode to the input parameters
@@ -75,14 +37,14 @@ void CreatePlaylistNode(PlaylistNode* thisNode, char idInit[], char songNameInit
  */
 void InsertPlaylistNodeAfter(PlaylistNode* thisNode, PlaylistNode* newNode)
 {
-    PlaylistNode* tempNext = NULL;
+    //PlaylistNode* tempNext = NULL;
     //1) Set temp to thisNode->nextNodePtr 
     //2) update thisNode->nextNodePtr to newNode
     //3) Set newNode->nextNodePtr to tempNext
     return;
 }
 
-void SetNextPlaylistNode()
+void SetNextPlaylistNode(PlaylistNode* thisNode, PlaylistNode* newNode)
 {
     //1) Set the thisNode->nextNodePtr to newNode
     return;
--- mod10/Pmain.c
+++ mod10/Pmain.c
@@ -9,7 +9,7 @@
  *        Version:  1.0
  *        Created:  03/25/2019 08:35:44 PM
  *       Revision:  none
- *       Compiler:  gcc Pmain.c -o Pmain.out
+ *       Compiler:  gcc Pmain.c PlaylistNode.c -o Pmain.out
  *          Usage:  ./Pmain.out
  *         Author:  Jayden Williams (), jaydenwilliams@mail.weber.edu
  *   Organization:  WSU
@@ -17,6 +17,8 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "PlaylistNode.h"
 // Constants
 
@@ -25,11 +27,16 @@
 // Main Function
 int main()
 {
+    //1) Prompt user for playlist title. Use fgets, don't forget to eliminate
+    //the end-of-line char
+    //
+    //2) Output a playlist menu of options
     char playlist[50];
     char userInput = '-';
 
     printf("Enter playlist's title:\n");
-    scanf("%s", playlist);
+    fgets(playlist, 50, stdin);
+    playlist[strlen(playlist) - 1] = '\0';
 
     while(userInput != 'q')
     {
@@ -38,5 +45,44 @@ int main()
     return 0;
 }
 // Function Definitions
+char PrintMenu(char playlist[])
+{
+    char userInput = '-';
+    char ID[50];
+    char songName[50];
+    char artistName[50];
+    int length;
+
+    printf("%s PLAYLIST MENU\n", playlist);
+    printf("a - Add song\n");
+    printf("r - Remove song\n");
+    printf("c - Change position of song\n");
+    printf("s - Output songs by specific artist\n");
+    printf("t - Output total time of playlist (in seconds)\n");
+    printf("o - Output full playlist\nq - Quit\n\n");
+    while(userInput != 'a'&&userInput != 'r'&&userInput != 'c'&&userInput != 's'
+            &&userInput != 't'&&userInput != 'o'&&userInput != 'q')
+    {
+        printf("Choose an option:\n");
+        scanf(" %c", &userInput);
+    }
+    switch(userInput)
+    {
+        case 'a':
+
+            printf("ADD SONG\n");
+            printf("Enter song's unique ID:\n");
+            scanf(" %s", ID);
+            printf("Enter song's name:\n");
+            scanf(" %s", songName);
+            printf("Enter artist's name:\n");
+            scanf(" %s", artistName);
+            printf("Enter song's length:\n");
+            scanf("%d", &length);
+
+    }
+    return userInput;
+}
+
 
 
