85d5ef871b22e98bcd6df73b592a892ec937cfb2
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-04-09 09:31:55 -0600

    d

---
+++ mod13/dummy.cpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  dummy.cpp
+ *
+ *    Description:  :w
+ *
+ *
+ *        Version:  1.0
+ *        Created:  04/09/2019 09:31:13 AM
+ *       Revision:  none
+ *    Compiler(C):  gcc dummy.cpp -o dummy.out
+         Compiler(C++):  g++
+ *          Usage:  ./dummy.out
+ *         Author:  Jayden Williams (), jaydenwilliams@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */ // for C
+#include <stdio.h>
+#include <stdlib.h>
+
+//For C++
+#include <iostream>
+using namespace std;
+
+
+// Constants and Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
+
+
