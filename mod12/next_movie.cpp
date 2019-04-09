5b78eceede01b8229abf428e9bcfdeff418cc95d
jayden7734 <46280933+jayden7734@users.noreply.github.com>
2019-04-09 08:45:39 -0600

    Create next_movie.cpp

---
+++ mod12/next_movie.cpp
@@ -0,0 +1,69 @@
+/*
+
+ * =====================================================================================
+
+ *
+
+ *       Filename:  _movie.cpp
+
+ *
+
+ *    Description:  Continue to use the Movie library
+
+ *
+
+ *        Version:  1.0
+
+ *        Created:  04/09/2019 08:40:08 AM
+
+ *       Revision:  none
+
+ *       Compiler (C++):  g++ next_movie.cpp Movie.cpp -o next_movie.out
+
+ *          Usage:  ./next_movie.out 
+
+ *
+
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+
+ *   Organization:  WSU
+
+ *
+
+ * =====================================================================================
+
+ */
+
+// For C++ Code
+
+#include <iostream>
+
+using namespace std;
+
+#include "Movie.h"
+
+
+
+
+
+// Constants and Globals
+
+
+
+// Function Prototypes
+
+
+
+// Main Function
+
+int main(int argc, char* argv[])
+
+{
+
+
+
+    return 0;
+
+}
+
+// Function Defenitions
