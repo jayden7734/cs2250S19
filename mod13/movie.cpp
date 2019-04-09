3981de418329dc82006b3d1eb1a3bd448466df13
Jayden Williams <jaydenwilliams@mail.weber.edu>
2019-04-09 10:22:43 -0600

    Add view list function

--- mod13/movie.cpp
+++ mod13/movie.cpp
@@ -1,173 +1,87 @@
 /*
-
  * =====================================================================================
-
  *
-
  *       Filename:  Movie.cpp
-
  *
-
  *    Description:  Movie Class
-
  *
-
  *        Version:  1.0
-
  *        Created:  04/04/2019 09:38:40 AM
-
  *       Revision:  none
-
  *       Compiler (C++):  g++ Movie.cpp -o Movie.out -lm
-
  *          Usage:  ./Movie.out 
-
  *
-
  *         Author:  Hugo Valle (), hugovalle1@weber.edu
-
  *   Organization:  WSU
-
  *
-
  * =====================================================================================
-
  */
-
 // For C++ Code
-
 #include <iostream>
-
 #include <iomanip>  // more cout options
-
-#include "Movie.h"
-
+#include "movie.h"
 using namespace std;
 
-
-
 Movie::Movie(string title, int year, int stars)
-
 {
-
     set_title(title);
-
     set_year(year);
-
     set_stars(stars);
-
 }
-
-
 
 // Destructor
-
 Movie::~Movie()
-
 {
-
     cout << "Destroying Object" << endl;
-
 }
-
-
 
 // Print info
-
 void Movie::info()const
-
 {
-
     int w = 10;
-
     cout << left
-
         << setw(w*3) << "TITLE"
-
         << setw(w) << "YEAR" << endl;
-
     cout << setw(w*3) << get_title()
-
         << setw(w) << get_year() << endl;
-
 }
-
 // Function Definitions
-
 void Movie::set_title(string set_title)
-
 {
-
 //    this->title = set_title;  // optional
-
     title = set_title;
-
 }
-
-
 
 void Movie::set_year(int year)
-
 {
-
     if (year < 1888)
-
     {
-
         throw invalid_argument("Year must be 1888 or later.");
-
     }
-
     // this-> refers to yourself
-
     // required because of the same name
-
     this->year = year;
-
 }
-
-
 
 void Movie::set_stars(int stars)
-
 {
-
     // this-> refers to yourself
-
     // required because of the same name
-
     this->stars = stars;
-
 }
-
-
 
 // Define Getters
-
 string Movie::get_title() const
-
 {
-
     return title;
-
 }
-
         
-
 int Movie::get_year() const
-
 {
-
     return year;
-
 }
-
-
 
 int Movie::get_stars() const
-
 {
-
     return stars;
-
 }
--- mod13/movie.h
+++ mod13/movie.h
@@ -1,99 +1,50 @@
 /*
-
  * =====================================================================================
-
  *
-
  *       Filename:  Movie.h
-
  *
-
  *    Description:  Movie Class Declaration
-
  *
-
  *        Version:  1.0
-
  *        Created:  04/04/2019 09:39:50 AM
-
  *       Revision:  none
-
  *       Compiler:  g++
-
  *
-
  *         Author:  Hugo Valle (), hugovalle1@weber.edu
-
  *   Organization:  WSU
-
  *
-
  * =====================================================================================
-
  */
-
 #ifndef  MOVIE__INC__
-
 #define  MOVIE__INC__
 
 
-
 #include <iostream>
-
 #include <string>
-
 using namespace std;
 
-
-
 class Movie
-
 {
-
     // Only the member has access to  data members are private
-
     private:
-
         string title;
-
         int year;
-
         int stars;
-
    // Anyone has access to it. Member functions are public
-
     public:
-
         // Constructors
-
         Movie(string title = "", int year = 1988, int stars = 0);
-
         // Destructor
-
         ~Movie();
-
         // "Setters"
-
         void set_title(string set_title);
-
         void set_year(int year);
-
         void set_stars(int stars);
-
         // "Getters"
-
         string get_title() const;
-
         int get_year() const;
-
         int get_stars() const;
-
         // Others
-
-        void info() const;
-
+       void info() const;
 }; // end of Movie class
-
-
-
-#endif /* ----- #ifndef MOVIE__INC__ ----- */
+#endif  /*#ifndef MOVIE__INC__ ----- */
